cd8bb20c6e53ea64b1b2f2af019d3c1706c63fd5
amit-prajapati-ap <prajapatiapamit@gmail.com>
2025-07-18 23:42:21 +0530

    Add new SVG icons and implement Sidebar component with dynamic behavior
    - Added new SVG assets: qrcode.png, regenerate_icon.svg, search_icon.svg, sidebar_close_icon.svg, sidebar_icon.svg, three_dots.svg.
    - Created Sidebar component with responsive design and dynamic expansion/collapse functionality.
    - Integrated new icons into the Sidebar for improved UI/UX.
    - Implemented hover effects and tooltips for better user interaction.

---
+++ app/page.jsx
@@ -0,0 +1,42 @@
+'use client'
+import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [expand, setExpand] = useState(false)
+  const [messages, setMessages] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <div>
+      <div className="flex h-screen">
+        
+        <Sidebar expand={expand} setExpand={setExpand}/>
+
+        <div className="flex flex-col flex-1 h-full items-center justify-center px-4 pb-8 text-white relative bg-[#292a2d]">
+          <div className="md:hidden px-4 absolute top-6 flex items-center justify-between w-full">
+            <Image onClick={() => (expand ? setExpand(false): setExpand(true))} className="rotate-180" src={assets.menu_icon} width={24} height={24} alt="sidebar_icon"/>
+            <Image className="opacity-70" src={assets.chat_icon} width={24} height={24} alt="sidebar_icon"/>
+          </div>
+
+          {messages.length === 0 ? (
+            <>
+              <div className="flex items-center gap-3">
+                <Image src={assets.logo_icon} alt="logo_icon" className="h-16"/>
+                <p className="text-2xl font-medium">Hi, I'm Promptify</p>
+              </div>
+              <p className="text-sm mt-2">How can I help you today</p>
+            </>
+          ) : (
+            <div></div>
+          )}
+
+          {/* Prompt box */}
+          <p className="text-xs absolute bottom-1 text-gray-500">AI-generated, for reference only</p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Sidebar.jsx
@@ -0,0 +1,113 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+const Sidebar = ({ expand, setExpand }) => {
+  return (
+    <div
+      className={`flex flex-col justify-between pt-7 transition-all duration-300 z-50 max-md:absolute max-md:h-screen bg-[#212327] ${
+        expand ? "p-4 w-64" : "md:w-20 w-0 max-md:overflow-hidden"
+      }`}
+     >
+      <div>
+        <div
+          className={`flex ${
+            expand ? "flex-row gap-10" : "flex-col items-center gap-8"
+          }`}
+          >
+          <Image
+            src={expand ? assets.logo_text : assets.logo_icon}
+            alt="logo_icon"
+            className={expand ? "w-36" : "w-10"}
+          />
+
+          <div
+            onClick={() => (expand ? setExpand(false) : setExpand(true))}
+            className="group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer"
+          >
+            <Image
+              src={assets.menu_icon}
+              alt="menu_icon"
+              className="md:hidden"
+            />
+            <Image
+              src={expand ? assets.sidebar_close_icon : assets.sidebar_icon}
+              alt="sidebar_icon"
+              className="hidden md:block w-7"
+            />
+            <div
+              className={`
+                absolute w-max px-3 py-2 rounded-lg shadow-lg bg-black text-white text-sm z-20
+                opacity-0 group-hover:opacity-100 transition pointer-events-none
+                ${
+                  expand
+                    ? "top-full mt-2 left-1/2 -translate-x-1/2"
+                    : "bottom-full mb-2 left-0"
+                }
+            `}
+            >
+              {expand ? "Close sidebar" : "Open sidebar"}
+              <div
+                className={`
+                    w-3 h-3 absolute bg-black rotate-45 z-10
+                    ${
+                      expand
+                        ? "left-1/2 -translate-x-1/2 -top-1.5"
+                        : "left-3 -bottom-1.5"
+                    }
+                `}
+              ></div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className={`mt-8 flex items-center justify-center cursor-pointer relative transition-all ${
+            expand
+              ? "bg-primary hover:opacity-90 rounded-2xl gap-2 px-4 py-2 w-max"
+              : "group h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"
+          }`}
+          >
+          <Image
+            src={expand ? assets.chat_icon : assets.chat_icon_dull}
+            alt="chat_icon"
+            className={expand ? "w-6" : "w-7"}
+          />
+
+          {expand && <span className="text-white font-medium">New chat</span>}
+
+          {!expand && (
+            <div
+              className="absolute w-max bottom-full mb-2 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100
+              transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none z-30"
+            >
+              New chat
+              <div className="w-3 h-3 absolute bg-black rotate-45 top-full left-1/2 -translate-x-1/2 -mt-1.5 z-20" />
+            </div>
+          )}
+        </button>
+
+        <div className={`mt-8 text-white/25 text-sm ${expand ? "block" : "hidden"}`}>
+          <p className="my-1">Recents</p>
+          {/* Chat label  */}
+        </div>
+      </div>
+
+      <div>
+        <div className={`flex items-center cursor-pointer group relative ${expand ? "gap-1 text-white/80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/10 cursor-pointer" : "h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+          <Image src={expand ? assets.phone_icon : assets.phone_icon_dull} alt="phone_icon" className={expand ? "w-5" : "w-6.5 mx-auto"}/>
+
+          <div className={`absolute -top-60 pb-8 ${!expand && "-right-40"} opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
+            <div className="relative w-max bg-black text-white text-sm p-3  rounded-lg shadow-lg">
+              <Image src={assets.qrcode} alt="qrcode" className={"w-44"}/>
+              <p>Scan to get Promptify App</p>
+              <div className={`w-3 h-3 absolute bg-black rotate-45 $  {expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
+            </div>
+          </div>
+        {expand && <><span>Get App</span> <Image src={assets.new_icon} alt="new_icon" /></>}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
